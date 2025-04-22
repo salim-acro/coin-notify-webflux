ef939b8ff0ef3858d182820f0e5ee971daaef078
lim <salim@acrofuture.com>
2025-04-22 16:50:34 +0900

    chore: 인덴트 조정 및 dependencies 추가

--- coin-notify/src/main/java/com/coin_notify/coin_notify/CoinNotifyApplication.java
+++ coin-notify/src/main/java/com/coin_notify/coin_notify/CoinNotifyApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoinNotifyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoinNotifyApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CoinNotifyApplication.class, args);
+    }
 }
